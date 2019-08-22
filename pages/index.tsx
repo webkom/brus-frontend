e4fea4b767ac29fc5296205e0a41aeeff02c1d14
Odin Ugedal <odin@ugedal.com>
2019-08-22 18:58:16 +0200

    Revert "Minor changes"
    This reverts commit 7c05aae158774abb50d4f8ba11ce8f2122f658ac.

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,18 +26,17 @@ const BrusGuiAsASingleFunction = () => {
   const [brusSuccess] = useMqttTopic(mqttServer, "notification/brus_success");
   const [brusError] = useMqttTopic(mqttServer, "notification/brus_error");
 
-  const noSleep = useRef<any>();
+  const noSleep = useRef<any>(() => new NoSleep());
   useEffect(() => {
-    noSleep.current = new NoSleep();
     document.addEventListener(
       "click",
       function enableNoSleep() {
         document.removeEventListener("click", enableNoSleep, false);
-        noSleep.current.enable();
+        noSleep.current().enable();
       },
       false
     );
-  }, [noSleep]);
+  }, []);
 
   useEffect(() => {
     sendShoppingCart(
