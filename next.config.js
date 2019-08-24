888f4a2b0e418f1887a31121747d12acf7d5bfba
Odin Ugedal <odin@ugedal.com>
2019-08-24 20:21:38 +0200

    Add babel polyfill

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 // next.config.js
+
 module.exports = {
-  target: 'serverless',
-}
+  target: "serverless",
+  webpack: function(cfg) {
+    const originalEntry = cfg.entry;
+    cfg.entry = async () => {
+      const entries = await originalEntry();
+
+      if (
+        entries["main.js"] &&
+        !entries["main.js"].includes("@babel/polyfill")
+      ) {
+        entries["main.js"].unshift("@babel/polyfill");
+      }
+
+      return entries;
+    };
+
+    return cfg;
+  }
+};
