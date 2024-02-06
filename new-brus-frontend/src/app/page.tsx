dca8629fce4a6ed92fcbeaecfcbcdaff87f4768f
Jenny Müller <jenny.mul@outlook.com>
2024-02-06 22:23:14 +0100

    improve code quality Co-Authored-By: Ingeborg <ingeborglundamo@gmail.com>

--- new-brus-frontend/src/app/page.tsx
+++ new-brus-frontend/src/app/page.tsx
@@ -4,29 +4,18 @@ import NextImage from 'next/image';
 import {
   Box,
   Button,
-  Card,
-  CardBody,
-  Center,
-  Grid,
-  GridItem,
   HStack,
   Heading,
-  IconButton,
-  Image,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  Text,
   VStack,
   useDisclosure,
 } from '@chakra-ui/react';
+import WallOfShameModal from '@/components/wallofshame-modal';
+import BuyBrusModal from '@/components/buy-brus-modal';
 
 export default function Home() {
-  const { isOpen, onClose, onOpen } = useDisclosure();
+  const wallOfShameDisclosure = useDisclosure();
+  const buyBrusDisclosure = useDisclosure();
+
   return (
     <VStack
       h={'100%'}
@@ -43,100 +32,30 @@ export default function Home() {
       >
         BRUS
       </Heading>
+
       <HStack wrap={'wrap'}>
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
-        <NextImage height={200} width={200} src={'/Profile.jpg'} />
+      <Box onClick={buyBrusDisclosure.onOpen}><NextImage alt={''} height={200} width={200} src={'/Profile.jpg'} /></Box>
       </HStack>
 
       <Button
         colorScheme="red"
         variant={'ghost'}
         fontSize="50px"
-        onClick={onOpen}
+        onClick={wallOfShameDisclosure.onOpen}
       >
         Wall of Shame
       </Button>
 
-      <Card backgroundColor={'blackAlpha.300'}>
-        <CardBody>
-          <Button colorScheme="red" variant={'ghost'} fontSize="50px">
-            X
-          </Button>
-
-          <HStack justify={'center'}>
-            <Button colorScheme="red" variant={'ghost'} fontSize="50px">
-              +
-            </Button>
-            <Heading fontSize="40px">4</Heading>
-            <Button colorScheme="red" variant={'ghost'} fontSize="50px">
-              -
-            </Button>
-          </HStack>
-          <HStack>
-            <VStack>
-              <Heading
-                marginRight="100px"
-                color={'black'}
-                textAlign={'right'}
-                marginTop={'4'}
-              >
-                Ta en til bitch
-              </Heading>
-              <IconButton
-              backgroundColor={'blackAlpha.300'}
-                icon={<Image src="/dahls.png" />}
-                aria-label={'Øl'}
-              />
-            </VStack>
-
-            <VStack>
-              <Card>
-                <Heading marginLeft="100px" color={'black'} textAlign={'left'}>
-                  Fyll på kassa
-                </Heading>
-                <IconButton
-                  icon={<Image height="100px" src="/dahlsBox.jpg" />}
-                  aria-label={'Øl'}
-                />
-              </Card>
-            </VStack>
-          </HStack>
-
-          <Heading textAlign={'center'}>Saldo:</Heading>
-        </CardBody>
-      </Card>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader textAlign="center" textColor="red" fontSize="50px">
-            Wall of Shame
-          </ModalHeader>
-          <Card>
-            <Text textAlign="center">Whats your excusee??</Text>
-            <HStack justify={'center'}>
-              <NextImage height={200} width={200} src={'/Profile.jpg'} />
-            </HStack>
-          </Card>
-          <ModalCloseButton />
-          <ModalBody>{/* <Lorem count={2} /> */}</ModalBody>
-
-          <ModalFooter></ModalFooter>
-        </ModalContent>
-      </Modal>
+      <BuyBrusModal
+        disclosure={buyBrusDisclosure}
+        text={'kuult'}
+        src={'/dahls.png'}
+      ></BuyBrusModal>
+      <WallOfShameModal
+        disclosure={wallOfShameDisclosure}
+        text={'kult'}
+        src="/Profile.jpg"
+      ></WallOfShameModal>
     </VStack>
   );
 }
---
+++ new-brus-frontend/src/components/amount-button.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@chakra-ui/react";
+
+export default function AmountButton(props: {text: string}){
+    return( 
+<Button colorScheme="red" variant={'ghost'} fontSize="50px">
+{props.text}
+</Button>
+)
+}
