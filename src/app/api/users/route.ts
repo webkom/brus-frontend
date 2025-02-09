9807a9a89b69f07fcff5e85552057c64800fa0c4
emillub <145599402+emillub@users.noreply.github.com>
2025-02-09 15:26:10 +0100

    fix drone

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,9 +2,14 @@ import { NextResponse } from "next/server";
 import { getUserCollection } from "../mongodb";
 import { User } from "@/app/utils/interfaces";
 
-export async function GET(req: Request) {
-  const user_collection = await getUserCollection();
-  const users: User[] = (await user_collection.find({}).toArray()) as User[];
+export async function GET() {
+  try {
+    const user_collection = await getUserCollection();
+    const users: User[] = (await user_collection.find({}).toArray()) as User[];
+    return NextResponse.json({ users: users }, { status: 200 });
+  } catch (error) {
+    console.error("Failed to parse JSON response:", error);
+  }
 
-  return new NextResponse(JSON.stringify(users));
+  return NextResponse.json({ users: [] }, { status: 200 });
 }
